fda27a328d73485ecc99c35a997bfb046b3fe243
abhirupkumar <abhirupkumar2003@gmail.com>
2024-12-10 19:27:44 +0530

    Enhance notification button styling with background and border

--- src/components/global/infobar/notifications/index.tsx
+++ src/components/global/infobar/notifications/index.tsx
@@ -3,7 +3,7 @@ import { Bell } from 'lucide-react'
 
 export const Notifications = () => {
   return (
-    <Button className="rounded-full py-6">
+    <Button className="rounded-full py-6 bg-background hover:bg-secondary border-[1px]">
       <Bell
         color="#3352CC"
         fill="#3352CC"
